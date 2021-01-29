faed05253bb70be0ca675d821c718e7eb3e6b3ff
hoorieh sabzevari <hoorieh95@gmail.com>
2021-01-29 15:44:41 -0800

    step2

--- main.cpp
+++ main.cpp
@@ -11,11 +11,23 @@ int main()
 	while (window.isOpen())
 	{
 		sf::Event event;
+		class ship
+		{
+		private:
+			sf::Texture texture;
+		public:
+			sf::Sprite sprite;
+			texture.loadFromFile("ship.png");
+			sprite.setTexture(texture);
+			sprite.setScale(0.2, 0.2);
+			sprite.setPosition(130, 500);
+		};
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
 			window.draw(sprite);
+			window.draw(ship.sprite);
 			window.display();
 		}
 	}
