a1f58ccbc3d20f6c4859c08b6f311301d4db216d
hoorieh sabzevari <hoorieh95@gmail.com>
2021-01-31 02:24:05 -0800

    update3

--- main.cpp
+++ main.cpp
@@ -3,32 +3,47 @@
 #include <SFML/Window.hpp>
 int main()
 {
-	sf::Texture texture;
-	texture.loadFromFile("stars.png");
-	sf::Sprite sprite;
-	sprite.setTexture(texture);
+	//Background
+	sf::Texture backtexture;
+	backtexture.loadFromFile("stars.png");
+	sf::Sprite backsprite;
+	backsprite.setTexture(backtexture);
+
+	//Ship
+	sf::Texture shiptexture;
+	sf::Sprite shipsprite;
+	shiptexture.loadFromFile("ship.png");
+	shipsprite.setTexture(shiptexture);
+	shipsprite.setScale(0.3, 0.3);
+	shipsprite.setPosition(130, 450);
+
+	//Enemy
+	sf::Texture enemytexture;
+	sf::Sprite enemysprite;
+	enemytexture.loadFromFile("enemy.png");
+	enemysprite.setTexture(enemytexture);
+	enemysprite.setScale(0.1, 0.1);
+	enemysprite.setPosition(130, 100);
+
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Space Invaders!");
 	while (window.isOpen())
 	{
 		sf::Event event;
-		class ship
-		{
-		private:
-			sf::Texture texture;
-		public:
-			sf::Sprite sprite;
-			texture.loadFromFile("ship.png");
-			sprite.setTexture(texture);
-			sprite.setScale(0.2, 0.2);
-			sprite.setPosition(130, 500);
-		};
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
-			window.draw(sprite);
-			window.draw(ship.sprite);
+			//Draw
+			window.draw(backsprite);
+			window.draw(shipsprite);
+			window.draw(enemysprite);
 			window.display();
+
+			//Movement
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+				shipsprite.move(4, 0);
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+				shipsprite.move(-4, 0);
 		}
 	}
 	return 0;
